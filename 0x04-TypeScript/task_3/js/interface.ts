632e776d42fbd1e7ec2eae173cb9f83b270e1395
Nurudin Wahab Lartey <nurundin2010@gmail.com>
2025-10-22 16:47:05 +0000

    ambinet namespaces

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,9 @@
+// Type RowID set equal to number
+export type RowID = number;
+
+// Interface RowElement with required fields
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
